99f010e4c991938c3276fe1bc6a30870747a8e23
TykkiMikk <32454232+TykkiMikk@users.noreply.github.com>
2018-02-12 16:45:49 +0800

    Update login_click.js

--- login_click.js
+++ login_click.js
@@ -3,9 +3,16 @@ function click(){
         password ="";
     account = document.getElementById("account").value;
     password = document.getElementById("password").value;
-    if(account == "123" && password == "123"){
-        alert("登陆成功");        
+    if(account != "123"){
+        alert("账号错误");
+        clearInput("password");
+    }else if(password != "123"){
+        alert("密码错误");
+        clearInput("account");
     }else{
-        alert("登陆失败");
-        }
+         alert("登陆成功");
     }
+
+function clearInput(id) {
+    document.getElementById(id).value = "";
+}
